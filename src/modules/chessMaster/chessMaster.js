44ddcbd17307789f080569a1e48c88ce8893c2a5
mbright90 <michael.bright90@icloud.com>
2022-11-06 10:44:46 +0000

    Add confitional checks within findAllMoves

--- src/modules/chessMaster/chessMaster.js
+++ src/modules/chessMaster/chessMaster.js
@@ -2,6 +2,7 @@ export default class ChessMaster {
   constructor() {
     this.startingPos = null
     this.endingPos = null
+    this.boardSize = 64
     this.knightMoveMatrix = {}
     this.xKnightMoves = [-2, -2, -1, -1, 1, 1, 2, 2]
     this.yKnightMoves = [-1, 1, -2, 2, -2, 2, -1, 1]
@@ -9,12 +10,16 @@ export default class ChessMaster {
 
   // Takes the position of the specified cell and returns all the possible knight moves as an array
   findAllMoves(x, y) {
+    const upperBound = Math.sqrt(this.BoardSize)
     const knightsArray = []
     for (let i = 0; this.xKnightMoves.length > i; i += 1) {
       const xPos = x + this.xKnightMoves[i]
       const yPos = y + this.yKnightMoves[i]
-      const newMove = [xPos, yPos]
-      knightsArray.push(newMove)
+      // Check the new x and new y positions are within bounds of chessBoard
+      if (!xPos >= upperBound || !xPos < 0 || !yPos >= upperBound || !yPos < 0) {
+        const newMove = [xPos, yPos]
+        knightsArray.push(newMove)
+      }
     }
     return knightsArray
   }
