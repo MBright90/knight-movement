fb6d38843995d2f973057fa4470957a526d50f12
mbright90 <michael.bright90@icloud.com>
2022-11-09 12:03:19 +0000

    Refactor class construction and private functions

--- src/modules/chessMaster/chessMaster.js
+++ src/modules/chessMaster/chessMaster.js
@@ -3,13 +3,14 @@ export default class ChessMaster {
     this.startingPos = null
     this.endingPos = null
     this.boardSize = 64
-    this.knightMoveMatrix = {}
     this.xKnightMoves = [-2, -2, -1, -1, 1, 1, 2, 2]
     this.yKnightMoves = [-1, 1, -2, 2, -2, 2, -1, 1]
+
+    this.knightMoveGraph = this.#createKnightMoveGraph()
   }
 
   // Takes the position of the specified cell and returns an array of all the possible knight moves
-  findAllMoves(x, y) {
+  #findAllMoves(x, y) {
     const upperBound = Math.sqrt(this.boardSize)
     const knightsArray = []
     for (let i = 0; i < this.xKnightMoves.length; i += 1) {
@@ -25,19 +26,22 @@ export default class ChessMaster {
 
   // Takes this.boardSize and creates an object, with cell coordinates as keys and an array of
   // possible knight moves as values; stored as this.knightMoveMatrix
-  resetKnightMoveMatrix() {
-    this.knightMoveMatrix = {}
+  #createKnightMoveGraph() {
+    const moveObject = {}
     const boardLength = Math.sqrt(this.boardSize)
     for (let i = 0; i < boardLength; i += 1) {
       for (let j = 0; j < boardLength; j += 1) {
-        this.knightMoveMatrix[`${i}${j}`] = this.findAllMoves(i, j)
+        moveObject[`${i}${j}`] = this.#findAllMoves(i, j)
       }
     }
+    console.log(moveObject)
+    return moveObject
   }
 
   // Setter functions
   setBoardSize(newSize) {
     this.boardSize = newSize
+    this.knightMoveGraph = this.#createKnightMoveGraph
   }
 
   // Getter functions
