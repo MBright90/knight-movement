bcf419d74df7d1ebb300784e4bbc45e5c6ed0546
mbright90 <michael.bright90@icloud.com>
2022-11-09 16:19:50 +0000

    Alter event listener creation

--- src/index.js
+++ src/index.js
@@ -5,3 +5,25 @@ import './styles/style.css'
 const master = new ChessMaster()
 console.log(master)
 dom.createNewBoard(64)
+
+function setKnightImageListeners() {
+  const allCells = document.querySelectorAll('.chess-cell')
+  allCells.forEach((cell) => {
+    cell.addEventListener('mouseover', dom.addBackgroundKnight)
+  })
+
+  allCells.forEach((cell) => {
+    cell.addEventListener('mouseout', dom.removeBackgroundKnight)
+  })
+
+  allCells.forEach((cell) => {
+    cell.addEventListener('click', () => {
+      allCells.forEach((chessCell) => {
+        chessCell.removeEventListener('mouseover', dom.addBackgroundKnight)
+        chessCell.removeEventListener('mouseout', dom.removeBackgroundKnight)
+      })
+    })
+  })
+}
+
+setKnightImageListeners()
