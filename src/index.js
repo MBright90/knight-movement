043bc859e1337c05008d886e241a6652318ec091
mbright90 <michael.bright90@icloud.com>
2022-11-09 12:00:14 +0000

    Refactor page loading script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import './modules/utilities/indexDom'
+import dom from './modules/utilities/indexDom'
 import ChessMaster from './modules/chessMaster/chessMaster'
 import './styles/style.css'
 
 const master = new ChessMaster()
-master.resetKnightMoveMatrix()
+console.log(master)
+dom.createNewBoard(64)
