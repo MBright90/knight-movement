f6e8ca56918efceeb1e35dedad34a4fd897414c7
mbright90 <michael.bright90@icloud.com>
2022-11-11 10:31:28 +0000

    Create function structure for knightMoveRecursive

--- src/modules/chessMaster/chessMaster.js
+++ src/modules/chessMaster/chessMaster.js
@@ -18,7 +18,7 @@ export default class ChessMaster {
       const yPos = y + this.yKnightMoves[i]
       // Check the new x and new y positions are within bounds of the current chessBoard
       if (xPos < upperBound && xPos >= 0 && yPos < upperBound && yPos >= 0) {
-        knightsArray.push([xPos, yPos])
+        knightsArray.push(`${xPos}${yPos}`)
       }
     }
     return knightsArray
@@ -35,6 +35,30 @@ export default class ChessMaster {
       }
     }
     return moveObject
+  }
+
+  knightMoveRecursive(currentCoord, targetCoord, takenMoves = []) {
+    // If the current coord matches the target coord return the target coord
+    if (currentCoord === targetCoord) return [targetCoord]
+    // If the current coord has already been checked within this loop return -1
+    if (takenMoves.includes(currentCoord)) return false
+
+    // Add the current position to the taken Moves array
+    takenMoves[takenMoves.length] = currentCoord
+
+    // Find the array of moves for the current position and recursively call function on each
+    // Returns -1 if that move has already been taken
+    const currentMoveSet = this.knightMoveGraph[currentCoord]
+
+    // Create an array to compare the return values of each possible move set
+    const compareArr = []
+    currentMoveSet.forEach((move) => {
+      const result = this.knightMoveRecursive(move, targetCoord, takenMoves)
+      if (result) compareArr[compareArr.length] = result
+    })
+    compareArr.sort((arrA, arrB) => arrA.length - arrB.length)
+    // Return the current shortest path to the target coord
+    return compareArr[0]
   }
 
   // Setter functions
