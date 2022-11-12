a670264726b5f2e4e7b853d716d1c4cb4420d904
mbright90 <michael.bright90@icloud.com>
2022-11-12 14:03:51 +0000

    Update knightMovesRecursive call for testing

--- src/index.js
+++ src/index.js
@@ -113,4 +113,4 @@ function resetAll() {
 resetButton.addEventListener('click', resetAll)
 
 setKnightImageListeners()
-master.knightMoveRecursive('22')
+console.log(master.knightMoveRecursive('22', '10'))
